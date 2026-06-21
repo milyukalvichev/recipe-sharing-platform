b1ec248e4e597aff02f7b5bef9b62942c1fdeb5b
milyukalvichev <mileto.kalvichev@gmail.com>
2026-06-21 14:12:37 +0300

    chore: initialize spring boot project with gradle and dependencies

---
+++ src/main/java/com/exam/recipeplatform/RecipeplatformApplication.java
@@ -0,0 +1,13 @@
+package com.exam.recipeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/recipeplatform/RecipeplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.recipeplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
